2c36d1b922fef9c696c1ce14c6582a3a91118310
masafumi kato <xslash1105@gmail.com>
2020-11-21 11:21:19 +0900

    [update] todo list updated using proptypes

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,14 +1,69 @@
-import React from 'react'
-// ここからスタート
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import TodoTextInput from './TodoTextInput';
 
-class TodoItem extends React.Component {
+export default class TodoItem extends Component {
+    static propTypes = {
+        todo: PropTypes.object.isRequired,
+        editTodo: PropTypes.func.isRequired,
+        deleteTodo: PropTypes.func.isRequired,
+        completeTodo: PropTypes.func.isRequired,
+    }
+
+    state = {
+        editing: false,
+    }
+
+    handleDoubleClick = () => {
+        this.setState({ editing: true })
+    }
+
+    handleSave = (id, text) => {
+        if (text.length === 0) {
+            this.props.deleteTodo(id);
+        } else {
+            this.props.editTodo(id, text);
+        }
+        this.setState({ editing: false });
+    }
+
     render() {
-        const {title, body, ...props} = this.props;
+        const { todo, completeTodo, deleteTodo} = this.props;
+        let element
+        if (this.state.editing) {
+            element = (
+                <TodoTextInput 
+                    text={todo.text}
+                    editing={this.state.editing}
+                    onSave={(text) => this.handleSave(todo.id, text)}
+                />
+            )
+        } else {
+            element = (
+                <div className="view">
+                    <input
+                        className="toggle"
+                        type="checkbox"
+                        checked={todo.completed}
+                        onChange={() => completeTodo(todo.id)}
+                    />
+                    <label onDoubleClick={this.handleDoubleClick}>
+                        {todo.text}
+                    </label>
+                    <button
+                        className="destroy"
+                        onClick={() => deleteTodo(todo.id)} />
+                </div>
+            )
+        }
         return (
-            <div className="todo_item" {...props}>
-                <h3 className="todo_item-title">{title}</h3>
-                <div className="todo_item-body">{body}</div>
-            </div>
+            <li className={classnames({
+                completed: todo.completed,
+                editing: this.state.editing
+            })}>
+                {element}
+            </li>
         )
     }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import TodoItem from './TodoItem';
 
-function TodoList(todos) {
-    console.log(todos);
-    return (
-        <div className="todo_list">
-        </div>
-    )
+const TodoList = ({ filteredTodos, actions }) => {
+    <ul className="todo-list">
+        {filteredTodos.map(todo => {
+            <TodoItem key={todo.id} todo={todo} {...actions} />
+        })}
+    </ul>
+}
+
+TodoList.propTypes = {
+    filteredTodos: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        completed: PropTypes.bool.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired).isRequired,
+    actions: PropTypes.object.isRequired
 }
 
 export default TodoList;
